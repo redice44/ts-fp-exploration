6cb2220a261d25ceccd16a34bc0b62cf99a944a7
Matt Thomson <mthomson.lee@gmail.com>
2019-12-30 17:06:57 -0500

    Multiple arg curries

--- src/fp/curry.ts
+++ src/fp/curry.ts
@@ -1,9 +1,11 @@
-export const curryN = (n: number, fn: Function): Function =>
-  (arg1: unknown) => 
-    _curryN(n - 1, fn, [arg1]);
+export const curryN = (requiredArguments: number, fn: Function): Function =>
+  (...args: any[]) =>
+    requiredArguments < args.length
+      ? fn(...args)
+      : _curryN(requiredArguments, fn, [...args]);
 
-const _curryN = (n: number, fn: Function, args: any[]): Function =>
-  (arg: any) => 
-    n === args.length - 1
-      ? fn(...args, arg)
-      : _curryN(n - 1, fn, [...args, arg]);
+const _curryN = (requiredArguments: number, fn: Function, args: any[]): Function =>
+  (...calledArgs: any[]) =>
+    requiredArguments < args.length + calledArgs.length
+      ? fn(...args, ...calledArgs)
+      : _curryN(requiredArguments, fn, [...args, ...calledArgs]);
