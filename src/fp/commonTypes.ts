f4e0be6a36a5aaecc506a2f9fa9ce28e66cb49be
Matt Thomson <mthomson.lee@gmail.com>
2019-12-30 21:27:05 -0500

    Dict map

--- src/fp/commonTypes.ts
+++ src/fp/commonTypes.ts
@@ -1,3 +1,4 @@
 export type nullaryFn<T> = () => T;
 export type unaryFnUniform<T> = (arg1: T) => T;
 export type binaryFnUniform<T> = (arg1: T, arg2: T) => T;
+export type dict<T> = { [key: string]: T };
--- src/fp/map.ts
+++ src/fp/map.ts
@@ -1,13 +1,38 @@
 import { curryN } from './curry';
 import { reduce } from './reduce';
+import { dict } from './commonTypes';
 
 export const map = curryN(
   1,
-  <T>(mapFn: Function, input: T): T => {
+  <T, U>(mapFn: Function, input: T): U => {
     if (Array.isArray(input) && input.length > 1) {
       const [first, ...next] = input;
       return reduce(mapFn, mapFn(first), next);
     }
     return mapFn(input);
-  },
+  }
 );
+
+export const mapDict = curryN(
+  1,
+  <T, U>(mapFn: (arg: T) => U, input: dict<T>): dict<U> =>
+    _mapDict(mapFn, {}, Object.keys(input), input)
+);
+
+const _mapDict = <T, U>(
+  mapFn: (arg: T) => U,
+  result: dict<U>,
+  keys: string[],
+  input: dict<T>
+): dict<U> => {
+  if (!keys.length) {
+    return result;
+  }
+  const [firstKey, ...nextKeys] = keys;
+  return _mapDict(
+    mapFn,
+    { ...result, [firstKey]: mapFn(input[firstKey]) },
+    nextKeys,
+    input
+  );
+};
